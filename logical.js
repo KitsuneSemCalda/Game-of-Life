cf5b4be528cb299d97ee7402cbb84be7c8328a26
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-02-24 17:28:43 -0300

    [Feat]: using canvas unstead tables

--- logical.js
+++ logical.js
@@ -1,86 +1,79 @@
-let matriz_render = document.getElementById("logical-game")
-let grid;
-let cols = 43
-let rows = 173
+const canvas = document.getElementById("logical-game")
+const ctx = canvas.getContext("2d")
+const cols = 720;
+const rows = 720;
+const cellSize = 5;
 
-function generate_grid(cols, rows){
-  let arr = new Array(cols)
+let grid = generateGrid(cols, rows);
 
-  for (let i = 0; i < arr.length; i++ ){
-    arr[i] = new Array(rows)
+function generateGrid(cols, rows) {
+  const grid = new Array(cols);
+  for (let i = 0; i < cols; i++) {
+    grid[i] = new Array(rows).fill(0);
   }
-  
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      arr[i][j] = Math.floor(Math.random() * 2 )
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = Math.floor(Math.random() * 2);
     }
   }
-
-  return arr
+  return grid;
 }
 
-function draw(){
-  matriz_render.innerHTML = ""
-  let next = new Array(cols)
-
-  for (let i = 0; i < cols; i++){
-    let linha = document.createElement("tr")
-    next[i] = new Array(rows)
-    for (let j = 0; j < rows; j++){
-      let coluna = document.createElement("td")
-      coluna.textContent = grid[i][j]
-      
-      if (grid[i][j] == 0){
-        coluna.classList.add("dead-cell")
-      }
-      if (grid[i][j] == 1){
-        coluna.classList.add("live-cell")
-      }
-
-      linha.appendChild(coluna)
-    }
-    matriz_render.appendChild(linha)
-  }
-    
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      let neighbours = countNeighbours(grid, i, j)
-      
-      let state = grid[i][j]
-
-      if (state == 0 && neighbours == 3){
-        next[i][j] = 1
-      } else if (state == 1 && (neighbours < 2 || neighbours > 3)){
-        next[i][j] = 0
-      } else{
-        next[i][j] = state
+function drawGrid() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      const x = i * cellSize;
+      const y = j * cellSize;
+      if (grid[i][j] === 1) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(x, y, cellSize, cellSize);
+      } else {
+        ctx.fillStyle = "black";
+        ctx.fillRect(x, y, cellSize, cellSize);
       }
     }
   }
-
-  grid = next
-   
 }
 
-function countNeighbours(grid, x, y){
+function countNeighbours(grid, x, y) {
   let sum = 0;
-
-  for (let i = -1; i < 2; i++){
-    for (let j = -1; j < 2; j++){
-      let row = (x + i + cols) % cols
-      let col = (y + j + rows) % rows
-      sum += grid[row][col]
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      const col = (x + i + cols) % cols;
+      const row = (y + j + rows) % rows;
+      sum += grid[col][row];
     }
   }
-  sum -= grid[x][y]
-  return sum
+  sum -= grid[x][y];
+  return sum;
 }
 
-function start(){
-  grid = generate_grid(cols, rows)
-  setInterval(function(){
-    draw();
-  }, 25)
+function update() {
+  const next = new Array(cols);
+  for (let i = 0; i < cols; i++) {
+    next[i] = new Array(rows).fill(0);
+  }
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      const state = grid[i][j];
+      const neighbours = countNeighbours(grid, i, j);
+      if (state === 0 && neighbours === 3) {
+        next[i][j] = 1;
+      } else if (state === 1 && (neighbours < 2 || neighbours > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+  grid = next;
 }
 
-start()
+function loop() {
+  update();
+  drawGrid();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
